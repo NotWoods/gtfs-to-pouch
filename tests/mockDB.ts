f107abf56f07eb08e290e0167f1cde8208093429
Tiger Oakes <tigeroakes@gmail.com>
2017-05-21 10:20:17 -1000

    Worked on mock db for testing

--- tests/mockDB.ts
+++ tests/mockDB.ts
@@ -1,16 +1,89 @@
+type WithKey = PouchDB.Core.AllDocsWithKeyOptions;
+type WithKeys = PouchDB.Core.AllDocsWithKeysOptions;
+type WithinRange = PouchDB.Core.AllDocsWithinRangeOptions
+
 /**
  * Only get and allDocs are called by functions in the `read` folder.
  */
-export class MockDB<T> {
-	constructor(name: string) {
+export class MockDB<T extends { _id: string }> {
+	private docs: Map<string, T>
 
+	constructor(name: string) {
+		this.docs = new Map();
 	}
 
 	async get(id: string): Promise<T> {
-		return <any> null;
+		const doc = this.docs.get(id);
+		if (!doc) throw { status: 404, name: 'not_found', message: 'missing' };
+		return doc;
 	}
 
-	async allDocs(options: PouchDB.Core.AllDocsOptions): Promise<PouchDB.Core.AllDocsResponse<T>> {
-		return <any> null;
+	async allDocs(
+		options: WithKeys | WithinRange | WithKey
+	): Promise<PouchDB.Core.AllDocsResponse<T>> {
+		let total_rows: 0;
+
+		let filterFunc: (doc: T, index: number) => boolean = () => true;
+		if ((options as WithKeys).keys) {
+			const keys = new Set((options as WithKeys).keys);
+			filterFunc = doc => keys.has(doc._id);
+		} else if ((options as WithinRange).startkey && (options as WithinRange).endkey) {
+			const {
+				startkey, endkey,
+				inclusive_end = true
+			} = options as WithinRange;
+
+			if (inclusive_end !== false)
+				filterFunc = doc => doc._id >= startkey && doc._id <= endkey;
+			else
+				filterFunc = doc => doc._id >= startkey && doc._id < endkey;
+		} else if ((options as WithKey).key) {
+			const { key } = options as WithKey;
+			filterFunc = doc => doc._id === key;
+		}
+
+		if (options.skip) {
+			filterFunc = (doc, index) => filterFunc(doc, index) && index >= options.skip;
+		}
+
+		let rows = Array.from(this.docs.values())
+			.filter(filterFunc)
+			.map(doc => {
+				const meta = <any> { _id: doc._id, _rev: '1-xxx' };
+				const row = {
+					doc: <T & PouchDB.Core.AllDocsMeta> meta,
+					id: doc._id,
+					key: doc._id,
+					value: { rev: '1-xxx' },
+				};
+
+				if (options.include_docs) Object.assign(row.doc, doc);
+				return row;
+			});
+
+		if (options.limit) {
+			rows = rows.slice(0, options.limit);
+		}
+		if (options.descending) {
+			rows = rows.reverse();
+		}
+
+		return {
+			offset: options.skip || 0,
+			total_rows,
+			rows,
+		};
+	}
+
+	async bulkDocs(docs: T[]): Promise<PouchDB.Core.BasicResponse[]> {
+		return docs.map(doc => {
+			this.docs.set(doc._id, doc);
+
+			return {
+				ok: true,
+				id: doc._id,
+				rev: '1-xxx',
+			};
+		})
 	}
 }
